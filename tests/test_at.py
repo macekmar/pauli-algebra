52e29b1b43281aff4d5bb99af6b52826b3379224
Marjan Macek <macek.marjan@hotmail.com>
2025-03-25 23:28:30 +0100

    Add sampling at() tests

--- tests/test_at.py
+++ tests/test_at.py
@@ -9,7 +9,7 @@ ops = ["XXIYZY", "XZYIZX"]
 weights = [-2.0, 4.5]
 
 @pytest.mark.parametrize("inv_order", [True, False])
-def test_netket_at(inv_order):
+def test_at(inv_order):
 
     if inv_order:
         spins = [-1,1]
@@ -30,3 +30,14 @@ def test_netket_at(inv_order):
     si = si.reshape(2**N, 2**N, N)
     sj = sj.reshape(2**N, 2**N, N)
     assert np.linalg.norm(op_pa.at(si, sj) - op_pa.to_dense()) < 1e-15
+
+@pytest.mark.parametrize("inv_order", [True, False])
+def test_at_sampling(inv_order):
+    el = [
+        pa.PauliString(w, s, inverted_ordering=inv_order)
+        for w, s in zip(weights, ops)
+    ]
+    op_pa = pa.PauliOperator(strings=el)
+
+    si, sj, w = op_pa.sample(100)
+    assert np.linalg.norm(op_pa.at(si,sj) - w) < 1e-15
--- tests/test_compare_netket.py
+++ tests/test_compare_netket.py
@@ -47,3 +47,19 @@ def test_netket_at(inv_order):
     si = si.reshape(2**N, 2**N, N)
     sj = sj.reshape(2**N, 2**N, N)
     assert np.linalg.norm(op_pa.at(si, sj) - op_nk.to_dense()) < 1e-15
+
+
+@pytest.mark.parametrize("inv_order", [True, False])
+def test_netket_at_sampling(inv_order):
+    hilbert = nk.hilbert.Spin(s=1 / 2, N=N, inverted_ordering=inv_order)
+    op_nk = nk.operator.PauliStrings(hilbert, operators=ops, weights=weights)
+    el = [
+        pa.PauliString(w, s, inverted_ordering=hilbert._inverted_ordering)
+        for w, s in zip(weights, ops)
+    ]
+    op_pa = pa.PauliOperator(strings=el)
+
+    si, sj, w = op_pa.sample(100)
+    i = hilbert.states_to_numbers(si)
+    j = hilbert.states_to_numbers(sj)
+    assert np.linalg.norm(op_nk.to_dense()[i,j] - w) < 1e-15
